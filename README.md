c68e3f16aaf23922d465d0f127bd2de22de4c464
YaelCano <78746517+YaelCano@users.noreply.github.com>
2023-12-04 10:28:14 -0500

    feat: :sparkles: Challenge finished

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Reto 1: Fibonacci
+
+
 Los números de Fibonacci 𝐹𝐾 son una sucesión de números naturales definidos de la siguiente manera:
 En palabras simples, la sucesión de Fibonacci comienza con 0 y 1, y los siguientes términos siempre son la suma de los dos anteriores.
 En la siguiente tabla, podemos ver los números de Fibonacci desde el 0-ésimo hasta el duodécimo.
@@ -11,4 +13,4 @@ entrada un número entero n, y entregue como salida el n-ésimo número de Fibon
 
 2. Escriba un programa que reciba como entrada un número entero e indique si es o no un número de Fibonacci:
 
-3. Escriba un programa que muestres los m primeros números de Fibonacci, donde m es un número ingresado por el usuario:
+3.Escriba un programa que muestres los m primeros números de Fibonacci, donde m es un número ingresado por el usuario:
--- Reto-1.py
+++ Reto-1.py
@@ -1,3 +1,4 @@
+import math
 # Reto 1: Fibonacci
 # Los números de Fibonacci 𝐹𝐾 son una sucesión de números naturales definidos de la siguiente manera:
 # En palabras simples, la sucesión de Fibonacci comienza con 0 y 1, y los siguientes términos siempre son la suma de los dos anteriores.
@@ -7,26 +8,45 @@
 
 
 # 1.Escriba un programa que reciba como entrada un número entero n, y entregue como salida el n-ésimo número de Fibonacci:
-N = 0,1,2,3,4,5,6,7,8,9,10,11,12
-F = 0,1,1,2,3,5,8,13,21,34,55,89
-def fibonacci(n):
-    if n == 0:
-        return F[0]
-    elif n == 1:
-        return F[1]
-    else:
-        for i in range (len(F)):
-            if n == i+2:
-                return F[i+1]
-            fibonacci(10)
-            fibonacci(11)
-            fibonacci(12)
-            fibonacci(0)
-print                 
+def fibonacci_con_bucle(n):
+    fib = [0,1]
+    for i in range(2, n + 1):
+        fib.append(fib[i -1] + fib[i - 2])
+    return fib
+n = int(input("Elije el numero de Fibonacci: "))
+arreglo =  fibonacci_con_bucle(n)
+print(arreglo[n])
+# 2. Escriba un programa que reciba como entrada un número entero e indique si es o no un número de Fibonacci:
+def fibonacci_confirmacion(x):
+    fib = [0,1]
+    num = 2
+    while True:
+        num += 1
+        fib.append(fib[-1]+ fib[-2])
+        if fib[-1]== x:
+            return fib
+            break
+        if fib[-1] > x:
+            return False
+x = int(input("Digita el numero: "))
+bool_result = fibonacci_confirmacion(x)
+print(bool_result)
+
+if bool_result:
+    print(f"{x} Si es un numero de fibonacci")
+else:
+    print(f"{x} No es un numero de fibonacci")    
 
 
+# 3. Escriba un programa que muestres los m primeros números de Fibonacci, donde m es un número ingresado por el usuario:
+def fibonacci_numericos(g):
+    fib = [0,1]
 
-
+    for i in range(2, g):
+        fib.append(fib[i - 1] + fib[i -2])
+    return fib
+consecutivo = int(input("Digita el numero: "))
+print(fibonacci_numericos(consecutivo))
 
 
 
